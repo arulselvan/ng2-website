2075091e97d3200975fdaff50bf11e75aeb1bc3a
Arul Selvan <hcl@LP-AC87A337CC55.local>
2017-03-24 16:16:13 +0530

    added material design component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
-
+import { MaterialModule } from '@angular/material'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TopNavigationComponent } from './shared/top-navigation/top-navigation.component';
 import { PageComponent } from './page/page.component'
-
+import 'hammerjs';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent ,pathMatch:'full'},
@@ -28,6 +28,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- src/app/shared/top-navigation/top-navigation.component.ts
+++ src/app/shared/top-navigation/top-navigation.component.ts
@@ -1,5 +1,4 @@
 import {Component} from "@angular/core"
-
 @Component({
     selector:"app-top-navigation",
     templateUrl:"./top-navigation.component.html",
@@ -7,6 +6,20 @@ import {Component} from "@angular/core"
 })
 
 export class TopNavigationComponent {
+  selected = '';
+  items = [
+    {text: 'Refresh'},
+    {text: 'Settings'},
+    {text: 'Help', disabled: true},
+    {text: 'Sign Out'}
+  ];
 
+  iconItems = [
+    {text: 'Redial', icon: 'dialpad'},
+    {text: 'Check voicemail', icon: 'voicemail', disabled: true},
+    {text: 'Disable alerts', icon: 'notifications_off'}
+  ];
+
+  select(text: string) { this.selected = text; }
 }
 
